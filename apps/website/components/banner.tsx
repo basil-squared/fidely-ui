f26d77b64d5abb55cf3f13ded9b4e0848520372b
justice chimobi <chimobi.justice@gmail.com>
2025-11-24 19:13:33 +0100

    chore: update banner version

--- apps/website/components/banner.tsx
+++ apps/website/components/banner.tsx
@@ -9,7 +9,7 @@ export const Banner = () => {
     <Box bg="orange.9" p="2" w="full">
       <Center>
         <Text fontWeight="bold" color="white">
-          Fidely UI v1 beta is now available!. 🚀
+          Fidely UI v1 is now available!. 🚀
         </Text>
       </Center>
     </Box>
