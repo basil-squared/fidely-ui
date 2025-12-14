0c7cc5b304e6486ae504856494a89d2c6420cc5d
justice chimobi <chimobi.justice@gmail.com>
2025-12-14 12:23:33 +0100

    revert(marguee): top/bottom inset

--- packages/preset/src/theme/slot-recipe/marquee.recipe.ts
+++ packages/preset/src/theme/slot-recipe/marquee.recipe.ts
@@ -65,13 +65,13 @@ export const marqueeSlotRecipe = defineSlotRecipe({
         background: 'linear-gradient(to left, var(--edge-color), transparent)',
       },
       '&[data-side="top"]': {
-        insetInline: 0,
+        insetBlockStart: 0,
         height: 'var(--edge-size)',
         background:
           'linear-gradient(to bottom, var(--edge-color), transparent)',
       },
       '&[data-side="bottom"]': {
-        insetInline: 0,
+        insetBlockEnd: 0,
         height: 'var(--edge-size)',
         background: 'linear-gradient(to top, var(--edge-color), transparent)',
       },
